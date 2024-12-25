639bb5690ae7b2de25b695207e8aa4f6b95088ce
smit763 <smitrakholiya763@gmail.com>
2024-12-25 22:10:24 +0530

    adding additional information in about section

--- src/components/Aboute/Aboute.jsx
+++ src/components/Aboute/Aboute.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import heroImage from "../../assets/images/hero-avatar.jpg";
 import { TbBrandFiverr } from "react-icons/tb";
-import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
+import { IoCall } from "react-icons/io5";
 
 const Aboute = () => {
   return (
@@ -62,7 +63,8 @@ const Aboute = () => {
               <li className="list-none inline-block">
                 <a
                   className="inline-block group w-[44px] h-[44px] rounded-full bg-white/15 text-white relative z-[1] overflow-hidden before:content-[''] before:absolute before:-z-[1] before:left-0 before:top-0 before:w-full before:h-full before:bg-themeGradient before:opacity-0 hover:before:opacity-20 before:transition-all before:ease-linear before:duration-100"
-                  href="#"
+                  target="_blank"
+                  href="https://www.linkedin.com/in/smit-rakholiya-4758b6262/"
                 >
                   <FaLinkedin className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 group-hover:top-0 group-hover:invisible group-hover:opacity-0" />
                   <FaLinkedin className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 invisible opacity-0 group-hover:top-1/2 group-hover:visible group-hover:opacity-100" />
@@ -71,10 +73,30 @@ const Aboute = () => {
               <li className="list-none inline-block">
                 <a
                   className="inline-block group w-[44px] h-[44px] rounded-full bg-white/15 text-white relative z-[1] overflow-hidden before:content-[''] before:absolute before:-z-[1] before:left-0 before:top-0 before:w-full before:h-full before:bg-themeGradient before:opacity-0 hover:before:opacity-20 before:transition-all before:ease-linear before:duration-100"
-                  href="#"
+                  target="_blank"
+                  href="mailto:smitrakholiya763@gmail.com"
                 >
-                  <BiLogoGmail className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 group-hover:top-0 group-hover:invisible group-hover:opacity-0"/>
-                  <BiLogoGmail className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 invisible opacity-0 group-hover:top-1/2 group-hover:visible group-hover:opacity-100"/>
+                  <BiLogoGmail className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 group-hover:top-0 group-hover:invisible group-hover:opacity-0" />
+                  <BiLogoGmail className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 invisible opacity-0 group-hover:top-1/2 group-hover:visible group-hover:opacity-100" />
+                </a>
+              </li>
+              <li className="list-none inline-block">
+                <a
+                  className="inline-block group w-[44px] h-[44px] rounded-full bg-white/15 text-white relative z-[1] overflow-hidden before:content-[''] before:absolute before:-z-[1] before:left-0 before:top-0 before:w-full before:h-full before:bg-themeGradient before:opacity-0 hover:before:opacity-20 before:transition-all before:ease-linear before:duration-100"
+                  target="_blank"
+                  href="https://wa.me/9664788220"
+                >
+                  <FaWhatsapp className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 group-hover:top-0 group-hover:invisible group-hover:opacity-0" />
+                  <FaWhatsapp className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 invisible opacity-0 group-hover:top-1/2 group-hover:visible group-hover:opacity-100" />
+                </a>
+              </li>
+              <li className="list-none inline-block">
+                <a
+                  className="inline-block group w-[44px] h-[44px] rounded-full bg-white/15 text-white relative z-[1] overflow-hidden before:content-[''] before:absolute before:-z-[1] before:left-0 before:top-0 before:w-full before:h-full before:bg-themeGradient before:opacity-0 hover:before:opacity-20 before:transition-all before:ease-linear before:duration-100"
+                  href="tel:+919664788220"
+                >
+                  <IoCall className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 group-hover:top-0 group-hover:invisible group-hover:opacity-0" />
+                  <IoCall className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ease-out duration-200 invisible opacity-0 group-hover:top-1/2 group-hover:visible group-hover:opacity-100" />
                 </a>
               </li>
             </ul>
