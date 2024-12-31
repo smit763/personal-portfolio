251699abcf3012e16c9a24fc61916c40c5255737
smit763 <smitrakholiya763@gmail.com>
2024-12-31 22:54:58 +0530

    adding images

--- src/Utils/Details/UserDetails.js
+++ src/Utils/Details/UserDetails.js
@@ -2,11 +2,11 @@ import heroImage from "../../assets/images/hero-avatar.jpg";
 import Resume from "../../assets/images/Docs/AshishGohilCV.pdf";
 
 export const usetDetails = {
-  userName: "Ashish Gohil",
-  mobileNumber: "9638156017",
-  email: "ashish.developer001@gmail.com",
+  userName: "Smit Rakholiya",
+  mobileNumber: "9664788220",
+  email: "smit.developer001@gmail.com",
   image: heroImage,
-  linkedIn: "https://www.linkedin.com/in/ashish-gohil-02b428166?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-  fiver: "https://www.fiverr.com/pe/kLAzxVL",
+  linkedIn: "https://www.linkedin.com/in/smit-rakholiya-4758b6262/",
+  fiver: "https://www.fiverr.com/smit__dev/",
   resume: Resume,
 };
